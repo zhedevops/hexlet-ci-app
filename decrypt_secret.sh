19e57bde483a93d5628fa189f38719aed0310bde
e.bulgakov@intec-balance.ru <e.bulgakov@intec-balance.ru>
2024-01-02 17:09:09 +0300

    Add new decryption script

---
+++ decrypt_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="snyk1234" \
+--output $HOME/secrets/token_snyk.txt token_snyk.txt.gpg
