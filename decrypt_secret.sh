a9bdb4f2696b685fd5ce0841c981d75543959083
e.bulgakov@intec-balance.ru <e.bulgakov@intec-balance.ru>
2024-01-02 20:25:12 +0300

    add snyk 13

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="snyk1234" --output $HOME/secrets/token_snyk.txt token_snyk.txt.gpg
-SNYK_TOKEN=`cat $HOME/secrets/token_snyk.txt`
+export SNYK_TOKEN=`cat $HOME/secrets/token_snyk.txt`
