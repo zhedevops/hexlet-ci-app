0a7f0430dc56765fddd48d136751236a094ea709
e.bulgakov@intec-balance.ru <e.bulgakov@intec-balance.ru>
2024-01-02 19:53:07 +0300

    add snyk 10

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,7 +4,6 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="snyk1234" \
---output $HOME/secrets/token_snyk.txt token_snyk.txt.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="snyk1234" --output $HOME/secrets/token_snyk.txt token_snyk.txt.gpg
 v=`cat $HOME/secrets/token_snyk.txt`
 echo "$v" >> $SNYK_TOKEN
